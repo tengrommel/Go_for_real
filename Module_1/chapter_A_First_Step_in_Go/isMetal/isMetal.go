48b7de46f7f163cff0a1de54ac548c9bc8fe9d23
teng <wwwsousoucom@126.com>
2017-11-08 23:31:22 +0800

    ismetall

---
+++ Module_1/chapter_A_First_Step_in_Go/isMetal/isMetal.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name = "Metalloids"
+	var triple = [3]int{5, 14, 84}
+	elements := []string{"Boron", "Silicon", "Polonium"}
+	isMetal := false
+	fmt.Println(name, triple, elements, isMetal)
+}
