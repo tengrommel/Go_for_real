4cfd1ac608190058b49b4c9ca9d647525e09c4de
teng <wwwsousoucom@126.com>
2017-11-11 20:55:27 +0800

    对goroutine的理解

---
+++ Module_1/chapter_concurrency/goroutine2/app.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	start := 0
+	stop := 50
+	step := 5
+
+	go func() {
+		count(start, stop, step)
+	}()
+
+	fmt.Scanln()
+}
+
+func count(start, stop, delta int)  {
+	for i:=start; i<=stop; i+=delta {
+		fmt.Println(i)
+	}
+}
