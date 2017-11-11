10757b1ee1e826826be68e8d681b8c75682899d8
teng <wwwsousoucom@126.com>
2017-11-11 21:26:10 +0800

    pattern

---
+++ Module_1/chapter_concurrency/pattern0/app.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"strings"
+	"fmt"
+)
+
+func main() {
+	data := []string{
+		"The yellow fish swims slowly in the water",
+		"The brown dog barks loudly after a drink from its water bowl",
+		"The dark bird of prey lands on a small tree after hunting for fish",
+	}
+
+	histogram := make(map[string]int)
+	done := make(chan bool)
+
+	go func() {
+		for _, line := range data {
+			words := strings.Split(line, " ")
+			for _, word := range words {
+				word = strings.ToLower(word)
+				histogram[word]++
+			}
+		}
+		done <- true
+	}()
+
+	if <-done{
+		for k, v := range histogram{
+			fmt.Printf("%s\t(%d)\n", k, v)
+		}
+	}
+}
