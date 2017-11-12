08543272e7c4b434ec35983d230c645edd47d8c6
tengrommel <wwwsousouccom@126.com>
2017-11-12 13:34:17 +0800

    sync

---
+++ Module_1/chapter_concurrency/sync1/sync.go
@@ -0,0 +1,30 @@
+package main
+
+import "time"
+
+type Service struct {
+	started bool
+	stpCh chan struct{}
+}
+
+func (s *Service)Start() {
+	s.stpCh = make(chan struct{})
+	go func() {
+		s.started = true
+		<-s.stpCh // wait to be closed.
+	}()
+}
+
+func (s *Service)Stop() {
+	if s.started{
+		s.started = false
+		close(s.stpCh)
+	}
+}
+
+func main() {
+	s := &Service{}
+	s.Start()
+	time.Sleep(time.Second)
+	s.Stop()
+}
