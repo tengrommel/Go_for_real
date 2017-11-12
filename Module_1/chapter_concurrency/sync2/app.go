d27d3e308b4b3472dfc21cf53fd7ac2a67819583
tengrommel <wwwsousouccom@126.com>
2017-11-12 13:41:09 +0800

    sync.mutex

--- Module_1/chapter_concurrency/sync2/app.go
+++ Module_1/chapter_concurrency/sync2/app.go
@@ -8,22 +8,22 @@ import (
 type Service struct {
 	started bool
 	stpCh chan struct{}
-	mutex sync.Mutex
+	sync.Mutex
 }
 
 func (s *Service)Start()  {
 	s.stpCh = make(chan struct{})
 	go func() {
-		s.mutex.Lock()
+		s.Lock()
 		s.started = true
-		s.mutex.Unlock()
+		s.Unlock()
 		<-s.stpCh
 	}()
 }
 
 func (s *Service)Stop()  {
-	s.mutex.Lock()
-	defer s.mutex.Unlock()
+	s.Lock()
+	defer s.Unlock()
 	if s.started{
 		s.started = false
 		close(s.stpCh)
