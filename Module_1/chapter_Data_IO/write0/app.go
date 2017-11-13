b0225ac8f02a5a39af194ad7e4ad50a96d29e678
tengrommel <wwwsousouccom@126.com>
2017-11-13 18:01:21 +0800

    更新管道

--- Module_1/chapter_Data_IO/write0/app.go
+++ Module_1/chapter_Data_IO/write0/app.go
@@ -2,15 +2,19 @@ package main
 
 import "fmt"
 
+// 声明管道并不声明管道长度
 type channelWriter chan byte
 
+// 注入方法
 func (c channelWriter)Write(p []byte) (int, error) {
 	if len(p) == 0 {
 		return 0, nil
 	}
 	count := 0
+	// 创建等待通道
 	wait := make(chan struct{})
 	go func() {
+		// 发送到 channelWriter 通道
 		for _, b := range p {
 			c <- b
 			count ++
@@ -18,6 +22,7 @@ func (c channelWriter)Write(p []byte) (int, error) {
 		close(c)
 		close(wait)
 	}()
+	// 等待进程
 	<-wait
 	return count,nil
 }
