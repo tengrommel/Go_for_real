e12b62c51f47009363287096868e405d89e2bea5
teng <wwwsousoucom@126.com>
2017-11-19 22:05:06 +0800

    golang的单例模式

--- Module_2/Chapter_Creational_Patterns/singleton/singleton.go
+++ Module_2/Chapter_Creational_Patterns/singleton/singleton.go
@@ -11,9 +11,13 @@ type singleton struct {
 var instance *singleton
 
 func GetInstance() Singleton {
-	return nil
+	if instance == nil {
+		instance = new(singleton)
+	}
+	return instance
 }
 
 func (s *singleton)AddOne() int {
-	return 0
+	s.count++
+	return s.count
 }
