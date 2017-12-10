6a5bc8431e61f4720b74a8dc92e2d30764134e95
tengrommel <wwwsousoucom@126.com>
2017-12-10 19:51:30 +0800

    asy

---
+++ Module_2/Chapter_Gos/asynchronous.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"sync"
+	"fmt"
+	"strings"
+)
+
+var wait sync.WaitGroup
+
+func main() {
+	wait.Add(1)
+
+	toUpperAsync("Hello Callbacks!", func(v string) {
+		fmt.Printf("Callback: %s\n", v)
+		wait.Done()
+	})
+
+	println("Waiting sync response...")
+	wait.Wait()
+}
+
+func toUpperAsync(word string, f func(string)) {
+	go func() {
+		f(strings.ToUpper(word))
+	}()
+}
